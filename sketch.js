d3cae8998d31b6d0cbbda80542a39392985c6a3b
Difei (Amber) Gao <dg3294@nyu.edu>
2023-09-20 15:01:50 -0400

    Print on console

--- sketch.js
+++ sketch.js
@@ -1,5 +1,21 @@
+let xpos = 180
+
 function setup() {
+
+    
+
+    createCanvas(600,600);
+    background(200,20,20);
 }
 
 function draw() {
+    
+
+    fill(0)
+    //rect(xpos, 10, 30, 30)
+    //rect(xpos, 10, 30, 30)
+    //rect(xpos, 10, 30, 30)
+     rect(mouseX, mouseY,30,30)
+     print(mouseX, mouseY);
+
 }
